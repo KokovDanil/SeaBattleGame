11f473f53de38829d09fa06f7fcbeaeaea169833
KokovDanil <danilkokov@yandex.ru>
2020-02-06 15:24:24 +0300

    1st commit

---
+++ src/model/Point.java
@@ -0,0 +1,33 @@
+package model;
+
+import java.util.Random;
+
+public class Point {
+    private int x;
+    private int y;
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public static Point getRandomPoint() {
+        return new Point(new Random().nextInt(10),new Random().nextInt(10));
+    }
+}
